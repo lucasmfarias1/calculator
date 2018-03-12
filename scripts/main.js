06c0018cc2356002585e57281110c5705ba7481d
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-12 20:31:52 -0300

    Implement every operator, every button functional and layout finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,64 @@
+const display = document.querySelector('#display');
+const operationDisplay = document.querySelector('#display-operation');
+const numButtons = document.querySelectorAll('button.number');
+const operators = document.querySelectorAll('button.operator');
+const clearButton = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const equals = document.querySelector('#equals');
+const negate = document.querySelector('#negate');
+const comma = document.querySelector('#comma');
+
+comma.onclick = () => {
+  if (display.innerHTML.match(/\./)) {
+    return;
+  } else {
+    display.innerHTML += '.';
+  }
+}
+
+negate.onclick = () => {
+  display.innerHTML = display.innerHTML * (-1);
+}
+
+backspace.onclick = () => {
+  display.innerHTML = display.innerHTML.slice(0, -1);
+  if (display.innerHTML.length < 2) {
+    display.innerHTML = display.innerHTML.replace(/[^\d]/, '');
+  }
+};
+
+clearButton.onclick = () => {
+  display.innerHTML = '';
+  operationDisplay.innerHTML = '';
+};
+
+operators.forEach((button) => {
+  button.onclick = (e) => {
+    if (operationDisplay.innerHTML == '' && display.innerHTML.length > 0){
+      operationDisplay.innerHTML = display.innerHTML + e.target.innerHTML;
+      display.innerHTML = '';
+    } else {
+      operationDisplay.innerHTML =
+          operationDisplay.innerHTML.replace(/[^d]$/, e.target.innerHTML);
+    }
+  }
+});
+
+numButtons.forEach((button) => {
+  button.onclick = (e) => {
+    display.innerHTML += e.target.innerHTML;
+  }
+});
+
+equals.onclick = () => {
+  if (operationDisplay.innerHTML == '' || display.innerHTML == '') return;
+  const operator = operationDisplay.innerHTML.slice(-1);
+  const a = Number(operationDisplay.innerHTML.slice(0, -1));
+  const b = Number(display.innerHTML);
+
+  display.innerHTML = operate(operator, a, b);
+  operationDisplay.innerHTML = '';
+};
 
 
 //Core calculating functions
