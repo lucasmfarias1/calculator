9d8dbdc5c24235cd8884194c8c1bdba056d16720
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-13 19:10:03 -0300

    Add keyboard support and disable backspace when non-numerical characters are on the display

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,9 @@ const comma = document.querySelector('#comma');
 
 let resultShowing = false;
 
-comma.onclick = () => {
+comma.onclick = inputComma;
+
+function inputComma() {
   if (display.innerHTML.match(/\./)) {
     return;
   } else if (display.innerHTML == '') {
@@ -24,45 +26,65 @@ negate.onclick = () => {
   display.innerHTML = display.innerHTML * (-1);
 }
 
-backspace.onclick = () => {
+backspace.onclick = eraseCharacter;
+
+function eraseCharacter() {
+  if (display.innerHTML.slice(1).match(/[^\.\d-+*/]/)) return;
+
   display.innerHTML = display.innerHTML.slice(0, -1);
   if (display.innerHTML.length < 2) {
     display.innerHTML = display.innerHTML.replace(/[^\d]/, '');
   }
-};
+}
 
-clearButton.onclick = () => {
+clearButton.onclick = clearDisplay;
+
+function clearDisplay() {
   display.innerHTML = '';
   operationDisplay.innerHTML = '';
-};
+}
 
 operators.forEach((button) => {
-  button.onclick = (e) => {
-    if (operationDisplay.innerHTML == '' && display.innerHTML.length > 0){
-      operationDisplay.innerHTML = display.innerHTML + e.target.innerHTML;
-      display.innerHTML = '';
-    } else {
-      operationDisplay.innerHTML =
-          operationDisplay.innerHTML.replace(/[^d]$/, e.target.innerHTML);
-    }
-  }
+  button.onclick = inputOperator;
 });
+
+function inputOperator(e) {
+
+  let key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (!key) key = e.target;
+
+  if (operationDisplay.innerHTML == '' && display.innerHTML.length > 0){
+    operationDisplay.innerHTML = display.innerHTML + key.innerHTML;
+    display.innerHTML = '';
+  } else {
+    operationDisplay.innerHTML =
+        operationDisplay.innerHTML.replace(/[^d]$/, key.innerHTML);
+  }
+}
 
 numButtons.forEach((button) => {
-  button.onclick = (e) => {
-    if (resultShowing == true) {
-      display.innerHTML = '';
-      resultShowing = false;
-    }
-    display.innerHTML += e.target.innerHTML;
-    if (display.innerHTML.length > 1 &&
-        display.innerHTML.match(/^0\d/)) {
-      display.innerHTML = display.innerHTML.slice(1);
-    }
-  }
+  button.addEventListener('click', inputNumber);
 });
 
-equals.onclick = () => {
+function inputNumber(e) {
+
+  let key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (!key) key = e.target;
+
+  if (resultShowing == true) {
+    display.innerHTML = '';
+    resultShowing = false;
+  }
+  display.innerHTML += key.innerHTML;
+  if (display.innerHTML.length > 1 &&
+      display.innerHTML.match(/^0\d/)) {
+    display.innerHTML = display.innerHTML.slice(1);
+  }
+}
+
+equals.onclick = getResult;
+
+function getResult() {
   if (operationDisplay.innerHTML == '' || display.innerHTML == '') return;
   const operator = operationDisplay.innerHTML.slice(-1);
   const a = Number(operationDisplay.innerHTML.slice(0, -1));
@@ -71,7 +93,27 @@ equals.onclick = () => {
   display.innerHTML = operate(operator, a, b);
   operationDisplay.innerHTML = '';
   resultShowing = true;
-};
+}
+
+function inputSomething(e) {
+  const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  console.log(key.id);
+  if (key.getAttribute('class').match('number')) {
+    inputNumber(e);
+  } else if (key.getAttribute('class').match('operator')) {
+    inputOperator(e);
+  } else if (key.getAttribute('id').match('equals')) {
+    getResult();
+  } else if (key.getAttribute('id').match('comma')) {
+    inputComma();
+  } else if (key.getAttribute('id').match('backspace')) {
+    eraseCharacter();
+  } else if (key.getAttribute('id').match('clear')) {
+    clearDisplay();
+  }
+}
+
+window.addEventListener('keydown', inputSomething);
 
 //Core calculating functions
 
